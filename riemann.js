4e04283038ffb1c844ab739999cebd20c3135d0d
James Butler <james.butler@sandfox.co.uk>
2014-08-18 17:05:34 +0100

    checkpoint commit

--- riemann.js
+++ riemann.js
@@ -1,6 +1,11 @@
 var Client = require('./riemann/client').Client;
+var Server = require('./riemann/server').Rie;
 
 // convenience method, how convenient.
 exports.createClient = function(options, onConnect) {
   return new Client(options, onConnect);
+};
+
+exports.createServer = function(options, onConnect) {
+  return new Server(options, onConnect);
 };
